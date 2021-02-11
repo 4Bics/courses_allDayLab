8c21b9a5c44c58b8490659de0abfce3445b240ee
Cameron Maclean <cm.maclean@yahoo.com>
2021-02-11 14:06:43 +0000

    Added seeds to data loader

--- src/test/java/com/example/codeclan/buildacourse/BuildacourseApplicationTests.java
+++ src/test/java/com/example/codeclan/buildacourse/BuildacourseApplicationTests.java
@@ -1,13 +1,40 @@
 package com.example.codeclan.buildacourse;
 
+import com.example.codeclan.buildacourse.models.Booking;
+import com.example.codeclan.buildacourse.models.Course;
+import com.example.codeclan.buildacourse.models.Customer;
+import com.example.codeclan.buildacourse.models.RatingType;
+import com.example.codeclan.buildacourse.repositories.BookingRepository;
+import com.example.codeclan.buildacourse.repositories.CourseRepository;
+import com.example.codeclan.buildacourse.repositories.CustomerRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootTest
 class BuildacourseApplicationTests {
 
+	@Autowired
+	CustomerRepository customerRepository;
+	@Autowired
+	BookingRepository bookingRepository;
+	@Autowired
+	CourseRepository courseRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void canAddThroughRepository(){
+		Customer customer = new Customer("Cameron", "Edinburgh", 25);
+		customerRepository.save(customer);
+
+		Course course = new Course("PSD", "Edinburgh", RatingType.FIVE);
+		courseRepository.save(course);
+
+		Booking booking = new Booking("28-02-21", customer, course);
+		bookingRepository.save(booking);
 	}
 
 }
