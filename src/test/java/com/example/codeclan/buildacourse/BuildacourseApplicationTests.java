68d75e316e9c95c48ca5626bf04e3b7758612730
Cameron Maclean <cm.maclean@yahoo.com>
2021-02-11 11:51:27 +0000

    Git init - added models, repos, controllers, components file structure

---
+++ src/test/java/com/example/codeclan/buildacourse/BuildacourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.buildacourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildacourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
