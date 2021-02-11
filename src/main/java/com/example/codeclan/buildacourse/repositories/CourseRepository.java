0441994f6d6c023e9c29e91cfcea44c8e1af1a58
Cameron Maclean <cm.maclean@yahoo.com>
2021-02-11 11:57:18 +0000

    Finished repositories - basic

--- src/main/java/com/example/codeclan/buildacourse/repositories/CourseRepository.java
+++ src/main/java/com/example/codeclan/buildacourse/repositories/CourseRepository.java
@@ -1,4 +1,9 @@
 package com.example.codeclan.buildacourse.repositories;
 
-public interface CourseRepository {
+import com.example.codeclan.buildacourse.models.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
 }
